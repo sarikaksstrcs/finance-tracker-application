aff7e36f6d6df3ba47bf40225cf2670058864a2d
K S Sarika <sarika.ks.official@gmail.com>
2025-03-26 13:55:30 +0530

    Backend Bug Fix

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,11 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 
 export const errorHandler = (
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     err: Error,
-    req: Request,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _req: Request,
     res: Response,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    next: NextFunction
+    _next: NextFunction
 ) => {
     console.error(err.stack);
     res.status(500).json({ error: err.message });
